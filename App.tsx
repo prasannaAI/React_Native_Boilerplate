49ce74b209211574c93ad7066e6660b689491d07
Prasanna <vjprasanna18@gmail.com>
2023-08-04 13:53:41 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import WelcomeScreen from './src/screen/WelcomeScreen';
+
+const App = () => {
+  return (
+    <View style={{flex: 1}}>
+      <WelcomeScreen />
+    </View>
+  );
+};
+
+export default App;
---
+++ src/screen/WelcomeScreen.js
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export default function WelcomeScreen() {
+  return (
+    <View className="flex-1 flex-row justify-center items-center">
+      <Text className="text-center text-amber-500">WelcomeScreen</Text>
+    </View>
+  );
+}
